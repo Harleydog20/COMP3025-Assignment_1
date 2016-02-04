3998a395a89f22f556bfc7f3f7d373b1891481ad
student <student@students-mac-7.student.georgianc.on.ca>
2016-02-04 17:35:30 -0500

    Added basic math functions

--- Calculator App/ViewController.swift
+++ Calculator App/ViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 //hi
 
 class ViewController: UIViewController {
+    
+    var isTypingNumber = false
+    var firstNumber:Int? = 0
+    var secondNumber:Int? = 0
+    var operation:String? = ""
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,15 +27,44 @@ class ViewController: UIViewController {
     }
 
     @IBOutlet weak var calcDisplay: UILabel!
+    
     @IBAction func numberTapped(sender: AnyObject) {
+        var number:String? = sender.currentTitle!
+        
+        if isTypingNumber {
+            calcDisplay.text = calcDisplay.text! + number!
+        } else {
+            calcDisplay.text = number
+            isTypingNumber = true
+        }
     }
 
     @IBAction func calculationTapped(sender: AnyObject) {
+        isTypingNumber = false
+        firstNumber = Int(calcDisplay.text!)
+        operation = sender.currentTitle!
+  
     }
     
     @IBAction func equlasTapped(sender: AnyObject) {
+        isTypingNumber = false
+        var result = 0
+        secondNumber = Int(calcDisplay.text!)
+        
+        if operation == "+" {
+            result = firstNumber! + secondNumber!
+        } else if operation == "-" {
+            result = firstNumber! - secondNumber!
+        } else if operation == "*" {
+            result = firstNumber! * secondNumber!
+        } else if operation == "/" {
+            result = firstNumber! / secondNumber!
+        }
+        
+        calcDisplay.text = "\(result)"
     }
     @IBAction func clearTapped(sender: AnyObject) {
+
     }
 }
 
